8469e8e583eaaa693b272eb0998d0cd34be4d7d1
CoolManTheCool <100152985+Kerbal-Man@users.noreply.github.com>
2022-09-26 13:39:09 -0500

    did something

--- main.cpp
+++ main.cpp
@@ -1,7 +1,4 @@
-#include "raylib.h"
-#include "methods.hpp"
-#include <iostream>
-#include <string>
+#include "theIncluder.hpp"
 
 int main(void){
     std::cout << "Special thanks to albert";
@@ -11,23 +8,37 @@ int main(void){
     int windowHeight = 540;
     InitWindow(windowWidth, windowHeight, "Planning Nova");
 
+    //window stats / flags
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
+
     //varibles init down
     const bool debug = true;
     bool exitWindow = false;
     int menuReturnInt;
+
+    //W O R L D
+    
+    game game;
+    
     //FPS Varibles
     int maxFPS = 60;
     int oldFPS = 0;
     int targetFPS = 0;
     int unfocusedFPS = 5;
-
+    
     //Enums
     enum class currentMenu { Game = 0, Title = 1, Paused = 2 };
     enum class f3Menu      { OFF = 0, F3 = 1, FPSCHART = 2, F3FPSCHART = 3 };
 
+    //Buttons
+    Rectangle mainMenu = { 0, 0, 0, 0,};
+    Rectangle quitGame = { 0, 0, 0, 0,};
+    Rectangle start    = { 0, 0, 0, 0,};
+    
+    //Textures / Images / Models / fonts
+    Font defaultFont = LoadFont("C:\\Users\\noahg\\source\\repos\\PlanningNova\\x64\\Debug\\Textures\\Fonts\\minecraft_font.ttf");
+
     //init misc.
-    Rectangle mainMenu = { 0.5 * windowWidth, 0.5 * windowHeight, .1 * windowWidth, .1 * windowHeight };
-    Rectangle quitGame = { 0.5 * windowWidth, 0.1 * windowHeight, .3 * windowWidth, .1 * windowHeight };
     f3Menu currentF3Mode;
     currentMenu menu = currentMenu::Title;
     currentF3Mode = f3Menu::OFF;
@@ -58,6 +69,12 @@ int main(void){
         if (menuReturnInt == 0) { menu = currentMenu::Game; }
         if (menuReturnInt == 2) { menu = currentMenu::Paused; }
         if (menuReturnInt == 3) { exitWindow = true; }
+        
+        windowHeight = GetScreenHeight();
+        windowWidth = GetScreenWidth();
+        mainMenu = { 0.1f * GetScreenWidth(), 0.5f  * GetScreenHeight(), 0.2f * GetScreenWidth(), 0.1f * GetScreenHeight() };
+        quitGame = { 0.1f * GetScreenWidth(), 0.8f  * GetScreenHeight(), 0.2f * GetScreenWidth(), 0.1f * GetScreenHeight() };
+        start    = { 0.1f * GetScreenWidth(), 0.05f * GetScreenHeight(), 0.2f * GetScreenWidth(), 0.1f * GetScreenHeight() };
 
         if (menu == currentMenu::Game) {
             //render game GUI
@@ -67,9 +84,15 @@ int main(void){
 
         if (menu == currentMenu::Title) {
             //render title menu
-            DrawRectangleRec(quitGame, PINK);
+            DrawRectangleRec(start, GRAY);
+            DrawTextEx(defaultFont, "Start", { start.x + 5, start.y }, 5 * GetScreenWidth() * GetScreenHeight() / 50000, 1, WHITE);
+            DrawRectangleRec(quitGame, GRAY);
             if (CheckCollisionPointRec(GetMousePosition(), quitGame) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
                 exitWindow = true;
+            }
+            if (CheckCollisionPointRec(GetMousePosition(), start) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+                menu = currentMenu::Game;
+                std::cout << "ALBERT IS DUMB\n";
             }
             EnableCursor();
             ShowCursor();
@@ -91,7 +114,9 @@ int main(void){
         //draw stuff in 2d 
 
         BeginMode3D(camera);
-        DrawGrid(100, 1.0f);
+        if (menu == currentMenu::Game) { 
+            DrawGrid(25, 1.0f); 
+        }
 
         EndMode3D();
 
